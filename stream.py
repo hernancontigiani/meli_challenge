7ab336a54e3720105d0bf8ffd736f791680f484f
hernancontigiani <hernan4790@gmail.com>
2020-06-28 17:32:18 -0300

    Implementar el sistema de stream

---
+++ stream.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+'''
+Modulo stream
+---------------------------
+Autor: Hernan Contigiani
+
+Descripcion:
+Modulo que contiene los diferentes tipos de
+stream que soporta el sistema
+'''
+
+__author__ = "Hernan Contigiani"
+__email__ = "hernan4790@gmail.com"
+__version__ = "1.0.0"
+
+import csv
+import json
+
+class Stream():
+    '''Virtual class for Stream model'''
+    def __init__(self, config):
+        self.id = None
+        self.fd = None
+        self.is_open = False
+        self.delimiter = config.get('delimiter',',')
+        self.encoding = config.get('encoding','utf-8')
+
+    def open(self, file_name):
+        try:
+            self.fd = open(file_name, 'r', encoding=self.encoding)
+            self.is_open = True
+        except:
+            self.is_open = False
+
+    def close(self):
+        if self.is_open is True:
+            self.fd.close()
+            self.is_open = False
+
+    def get_chunk(self, chunksize):
+        # Virtual method
+        raise NotImplementedError
+
+
+    def parse_line(self, line):
+        # Virtual method
+        raise NotImplementedError
+
+
+class CsvStream(Stream):
+    '''Class for CSV stream'''
+    def __init__(self, config):
+        super(CsvStream, self).__init__(config)
+        self.reader = None
+
+    def get_chunk(self, chunksize):
+        chunk = []
+        if self.is_open is False:
+            return chunk
+
+        if self.reader is None:
+            self.reader  = csv.DictReader(self.fd, self.delimiter)
+
+        for i in range(chunksize):
+            row = next(self.reader, None)
+            if row is None:
+                break
+            chunk.append(row)
+
+        return chunk
+
+    def parse_line(self, line):
+        id = line.get('id')
+        site = line.get('site')
+        return({'site': site, 'id': id})
+
+
+class TxtStream(Stream):
+    '''Class for TxT stream'''
+    def __init__(self, config):
+        super(TxtStream, self).__init__(config)
+
+    def get_chunk(self, chunksize):
+        chunk = []
+        if self.is_open is False:
+            return chunk
+
+        for i in range(chunksize):
+            row = self.fd.readline()
+            if row is None:
+                break
+            chunk.append(row)
+
+        return chunk
+
+    def parse_line(self, line):
+        data = line.split(self.delimiter)
+        if len(data) < 2:
+            return {}
+
+        site = data[0]
+        if data[1].isdigit() is True:
+            id = int(data[1])
+        else:
+            id = 0
+        return({'site': site, 'id': id})
+
+
+class Jsonlstream(Stream):
+    '''class for JSON Line stream'''
+    def __init__(self, config):
+        super(Jsonlstream, self).__init__(config)
+
+    def get_chunk(self, chunksize):
+        chunk = []
+        if self.is_open is False:
+            return chunk
+
+        for i in range(chunksize):
+            row = self.fd.readline()
+            if row is None:
+                break
+            chunk.append(row)
+
+        return chunk
+
+    def parse_line(self, line):
+        json1_str = line.replace('\n', '')
+        json1_data = json.loads(json1_str)
+
+        id = json1_data.get('id')
+        site = json1_data.get('site')
+
+        return({'site': site, 'id': id})
+
---
+++ stream_test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+'''
+Modulo de test stream
+---------------------------
+Autor: Hernan Contigiani
+
+Descripcion:
+unit test del modulo stream
+'''
+
+__author__ = "Hernan Contigiani"
+__email__ = "hernan4790@gmail.com"
+__version__ = "1.0.0"
+
+import unittest
+
+from stream import CsvStream, TxtStream, Jsonlstream
+
+
+class StreamTestCase(unittest.TestCase):
+    ''' Ensayo de los streams'''
+    def test_get_chunk(self):
+
+        # Test csv stream
+        file_name = 'technical_challenge_data.csv'
+
+        config = {'delimiter': ',', 'encoding': 'utf-8'}
+        csv_stream = CsvStream(config)
+        csv_stream.open(file_name)
+
+        # Check chunk size
+        chunksize = 2
+        chunk = csv_stream.get_chunk(chunksize=chunksize)
+        self.assertEqual(len(chunk), chunksize)
+
+        # Test txt stream
+        file_name = 'technical_challenge_data.txt'
+
+        config = {'delimiter': '|', 'encoding': 'utf-8'}
+        txt_stream = TxtStream(config)
+        txt_stream.open(file_name)
+
+        # Check chunk size
+        chunksize = 2
+        chunk = txt_stream.get_chunk(chunksize=chunksize)
+        self.assertEqual(len(chunk), chunksize)
+
+        # Jsonlstream
+        file_name = 'technical_challenge_data.jsonl'
+
+        config = {'delimiter': ',', 'encoding': 'utf-8'}
+        jsonl_stream = Jsonlstream(config)
+        jsonl_stream.open(file_name)
+
+        # Check chunk size
+        chunksize = 2
+        chunk = jsonl_stream.get_chunk(chunksize=chunksize)
+        self.assertEqual(len(chunk), chunksize)
+
+    def test_parse_line(self):
+        id_test = 905070
+        site_test = 'MLA'
+        dump = 'dump'
+
+        # Test csv stream
+        csv_dict_line = {'id': id_test, 'site': site_test, 'dump': dump}
+
+        config = {'delimiter': ',', 'encoding': 'utf-8'}
+        csv_stream = CsvStream(config)
+        chunk = csv_stream.parse_line(csv_dict_line)
+
+        self.assertEqual(chunk.get('id'), id_test)
+        self.assertEqual(chunk.get('site'), site_test)
+
+        # Test txt stream
+        config = {'delimiter': '|', 'encoding': 'utf-8'}
+        delimiter = config['delimiter']
+        txtline = site_test + delimiter + str(id_test)
+
+        txt_stream = TxtStream(config)
+        chunk = txt_stream.parse_line(txtline)
+
+        self.assertEqual(chunk.get('id'), id_test)
+        self.assertEqual(chunk.get('site'), site_test)
+
+        # Test jsonl stream
+        config = {'delimiter': ',', 'encoding': 'utf-8'}
+        # json_line = '{"site": "{}", "id": {}}\n'.format(site_test, id_test)
+        json_line = '{"site": "MLA", "id": 905070}\n'
+
+        jsonl_stream = Jsonlstream(config)
+        chunk = jsonl_stream.parse_line(json_line)
+
+        self.assertEqual(chunk.get('id'), id_test)
+        self.assertEqual(chunk.get('site'), site_test)
+
+
+if __name__ == '__main__':
+    unittest.main()
